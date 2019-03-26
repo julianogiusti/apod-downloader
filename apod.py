5cb18a9e9c566867bb0ad9b15e7d979623a6fa26
Juliano Giusti <juliano@inceres.com.br>
2019-03-26 17:06:19 -0300

    Creating log file

--- apod.py
+++ apod.py
@@ -13,8 +13,8 @@ class Apod(object):
         self.date = date
 
 
-    def get_date(self):
-        return datetime.now().strftime("%Y-%m-%d")
+    def get_time(self):
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
     def is_downloadable(self):
@@ -51,29 +51,67 @@ class Apod(object):
 
 
     def download_and_save_image(self, set_wallpaper=False):
-        self.__image = requests.get(self.__request_result.json()['hdurl'], allow_redirects=True)
-        filename = self.__request_result.json()['hdurl'].split("/")[6]
-        if self.date != "":
-            date = self.date
+        date = self.__request_result.json()['date']
+        media_type = self.__request_result.json()['media_type']
+        title = self.__request_result.json()['title']
+        apod_link = "https://apod.nasa.gov/apod/ap{}.html".format(date.replace("-", "")[2:])
+
+        if media_type == "image":
+            hdurl = self.__request_result.json()['hdurl']
+            self.__image = requests.get(hdurl, allow_redirects=True)
+
+            filename = title.replace(" ", "_")
+            image_path = '/home/juliano/Pictures/APOD/{}-{}'.format(date, filename)
+            open(image_path, 'wb').write(self.__image.content)
+            status = "downloaded"
+            if set_wallpaper:
+                os.system("gsettings set org.gnome.desktop.background picture-uri file://{}".format(image_path))
         else:
-            date = self.get_date()
-        file_path = '/home/juliano/Pictures/APOD/{}-{}'.format(date, filename)
-        open(file_path, 'wb').write(self.__image.content)
-        if set_wallpaper:
-            os.system("gsettings set org.gnome.desktop.background picture-uri file://{}".format(file_path))
+            status = "not_downloaded"
+        self.log_links(date, apod_link, media_type, status)
+
+
+    def log_links(self, date, link, type, status):
+        image_list_path = "/home/juliano/Pictures/APOD/images_list.csv"
+        colnames = ['date', 'link', 'type', 'status']
+        if (not os.path.isfile(image_list_path )):
+            with open(image_list_path , 'a') as file:
+                file.write(';'.join(map(str, colnames)))
+                file.write('\n')
+        row_content = "{};{};{};{}\n".format(date, link, type, status)
+        with open(image_list_path, "a") as file:
+            file.write(row_content)
+
+
+    def log_messages(self, message):
+        log_file_path = "/home/juliano/Pictures/APOD/log.txt"
+        with open(log_file_path, "a") as file:
+            file.write("{}: {}\n".format(self.get_time(), message))
+
 
 
 # date in format yyyy-mm-dd: 2019-01-01
 date = ""
 apod = Apod(date)
+apod.log_messages("Script started execution")
+print "Script started execution"
+
 apod.get_apod_data()
 
 if apod.is_downloadable():
     try:
         apod.download_and_save_image(set_wallpaper=True)
-        print "APOD image downloaded successfully, enjoy!!"
+        message = "APOD image downloaded successfully"
+        print message
+        apod.log_messages(message)
     except Exception as ex:
-        print "Oops, something went wrong, sorry about that :/"
-        print "EXCEPTION: {}".format(ex)
+        message = "Oops, something went wrong, sorry about that :/ - Exception: {}".format(ex)
+        print message
+        apod.log_messages(message)
 else:
-    print "Sorry, can't download file.\nProbably, the cause is that today at apod there is a video file, which this script still not handle, maybe in the future, who knows."
+    message = "Sorry, can't download file. Probably, the cause is that today at apod there is a video file, which this script still not handle, maybe in the future, who knows."
+    apod.log_messages(message)
+    print message
+
+apod.log_messages("Script finished execution")
+print "Script finished execution"
