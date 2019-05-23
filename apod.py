fdfe3bd68699253f93f7d4b3f1b0d8f032e35755
Juliano Cesar Giusti <giustijuliano@gmail.com>
2019-05-23 14:34:20 -0300

    Random date

--- apod.py
+++ apod.py
@@ -1,27 +1,16 @@
 import os
-import re
 import sys
 import requests
+import random
 from datetime import datetime
-
-def get_time():
-    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-
-def log_messages(message, apod_directory):
-    log_file_path = "{}log.txt".format(apod_directory)
-    message = "{}: {}\n".format(get_time(), message)
-    print message
-    with open(log_file_path, "a") as file:
-        file.write("{}".format(message))
-
 
 class Apod(object):
 
-    def __init__(self, date, apod_directory):
-        self.url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date={}".format(date)
-        self.apod_directory = apod_directory
+    def url(self):
+        return self.url
 
+    def apod_directory(self):
+        return self.apod_directory
 
     def download_and_save_image(self, set_wallpaper=False):
         request_result = requests.get(self.url)
@@ -43,28 +32,50 @@ class Apod(object):
             if set_wallpaper:
                 os.system("gsettings set org.gnome.desktop.background picture-uri file://{}".format(wallpaper_path))
             message = "APOD image downloaded successfully, APOD web page: {}".format(apod_link)
-            log_messages(message=message, apod_directory=self.apod_directory)
+            self.log_messages(message=message, apod_directory=self.apod_directory)
         else:
-            message = "APOD content isn't an image, so it's not being downloaded, APOD web page: {}".format(apod_link)
-            log_messages(message=message, apod_directory=self.apod_directory)
+            message = "APOD content isn't an image, generating random date to try again..."
+            self.log_messages(message=message, apod_directory=self.apod_directory)
+            self.url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date={}".format(self.random_date())
+            self.download_and_save_image()
+
+    def random_date(self):
+        start_date = datetime.today().replace(day=16, month=6, year=1995).toordinal()
+        end_date = datetime.today().toordinal()
+        date = datetime.fromordinal(random.randint(start_date, end_date)).strftime("%Y-%m-%d")
+        return date
+
+    def create_apod_directory(self):
+        self.apod_directory = "{}/APOD/".format(os.path.expanduser("~"))
+        if not os.path.exists(self.apod_directory):
+            try:
+                os.makedirs(self.apod_directory)
+                self.log_messages(message="Creating directory {} to store images from APOD".format(self.apod_directory), apod_directory=self.apod_directory)
+            except Exception as ex:
+                self.log_messages(message="Error trying to create APOD directory: {}".format(ex), apod_directory=self.apod_directory)
+                exit(1)
+
+    def log_messages(self, message, apod_directory):
+        log_file_path = "{}log.txt".format(apod_directory)
+        message = "{}: {}\n".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), message)
+        print message
+        with open(log_file_path, "a") as file:
+            file.write("{}".format(message))
+
+apod = Apod()
+apod.create_apod_directory()
 
 if len(sys.argv) > 1:
     date = str(sys.argv[1])
+    if date == "random":
+        date = apod.random_date()
 else:
-    date = datetime.now().strftime("%Y-%m-%d")
-apod_directory = "{}/APOD/".format(os.path.expanduser("~"))
-if not os.path.exists(apod_directory):
-    try:
-        os.makedirs(apod_directory)
-        log_messages(message="Creating directory {} to store images from APOD".format(apod_directory), apod_directory=apod_directory)
-    except Exception as ex:
-        log_messages(message="Error trying to create APOD directory: {}".format(ex), apod_directory=apod_directory)
-        exit(1)
-log_messages(message="Script started execution", apod_directory=apod_directory)
-apod = Apod(date, apod_directory)
+    date = datetime.today().strftime("%Y-%m-%d")
+
+apod.url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date={}".format(date)
+
 try:
     apod.download_and_save_image(set_wallpaper=True)
 except Exception as ex:
     message = "Oops, something went wrong, sorry about that :/ - Exception: {}".format(ex)
-    log_messages(message=message, apod_directory=apod_directory)
-log_messages("Script finished execution", apod_directory=apod_directory)
+    apod.log_messages(message=message, apod_directory=apod.apod_directory)
