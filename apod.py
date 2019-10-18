70628d614ccd095b4c377a1f37161b8b5676b5f1
Juliano Giusti <juliano@inceres.com.br>
2019-10-18 09:17:17 -0300

    Replace slash with underscore in filenames

--- apod.py
+++ apod.py
@@ -7,6 +7,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 class Apod(object):
 
     def __init__(self, url=None, log_file=None, log_days_file=None):
@@ -35,21 +36,22 @@ class Apod(object):
         date = request_response['date']
         media_type = request_response['media_type']
         title = request_response['title']
-        apod_link = "https://apod.nasa.gov/apod/ap{}.html".format(date.replace("-", "")[2:])
+        apod_link = f'https://apod.nasa.gov/apod/ap{date.replace("-", "")[2:]}.html'
 
         if media_type == "image":
             hdurl = request_response['hdurl']
             image = requests.get(hdurl, allow_redirects=True)
 
             filename = title.replace(" ", "_")
+            filename = filename.replace("/", "_")
             file_extension = hdurl.split("/")[-1].split(".")[-1]
-            image_path = '{}{}-{}.{}'.format(self.apod_directory, date, filename, file_extension)
-            wallpaper_path = '{}wallpaper.{}'.format(self.apod_directory, file_extension)
+            image_path = f'{self.apod_directory}{date}-{filename}.{file_extension}'
+            wallpaper_path = f'{self.apod_directory}wallpaper.{file_extension}'
             open(image_path, 'wb').write(image.content)
             open(wallpaper_path, 'wb').write(image.content)
             if set_wallpaper:
-                os.system("gsettings set org.gnome.desktop.background picture-uri file://{}".format(wallpaper_path))
-            message = "APOD image downloaded successfully, APOD web page: {}".format(apod_link)
+                os.system(f'gsettings set org.gnome.desktop.background picture-uri file://{wallpaper_path}')
+            message = f'APOD image downloaded successfully, APOD web page: {apod_link}'
             self.log_messages(message=message)
             self.log_days(date=date, title=title, link=apod_link)
         else:
@@ -74,7 +76,7 @@ class Apod(object):
                 date = self.random_date()
         else:
             date = datetime.today().strftime("%Y-%m-%d")
-        self.url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date={}".format(date)
+        self.url = f'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date={date}'
 
     def date_already_downloaded(self, date):
         days_list = pd.read_csv(self.log_days_file, sep=";")
@@ -84,27 +86,28 @@ class Apod(object):
         return False
 
     def create_apod_directory(self):
-        self.apod_directory = "{}/APOD/".format(os.path.expanduser("~"))
+        self.apod_directory = f'{os.path.expanduser("~")}/APOD/'
         if not os.path.exists(self.apod_directory):
             try:
                 os.makedirs(self.apod_directory)
-                self.log_messages(message="Creating directory {} to store images from APOD".format(self.apod_directory))
+                self.log_messages(message=f'Creating directory {self.apod_directory} to store images from APOD')
             except Exception as ex:
-                self.log_messages(message="Error trying to create APOD directory: {}".format(ex))
+                self.log_messages(message=f'Error trying to create APOD directory: {ex}')
 
     def log_messages(self, message):
-        message = "{}: {}\n".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), message)
+        message = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: {message}\n'
         with open(self.log_file, "a") as file:
-            file.write("{}".format(message))
+            file.write(f'{message}')
 
     def log_days(self, date, title, link):
         with open(self.log_days_file, "a") as file:
-            file.write("{};{};{}\n".format(date, title, link))
+            file.write(f'{date};{title};{link}\n')
 
 
 @app.route("/healthcheck")
 def healthcheck():
     return "it's alive!", 200
+
 
 @app.route("/apod_wallpaper")
 @app.route("/apod_wallpaper/<string:date>")
@@ -112,18 +115,19 @@ def apod_wallpaper(date=None):
     apod = Apod()
     try:
         apod.create_apod_directory()
-        apod.log_file = "{}log.txt".format(apod.apod_directory)
-        apod.log_days_file = "{}log_days.csv".format(apod.apod_directory)
-        if (not os.path.isfile(apod.log_days_file)):
+        apod.log_file = f'{apod.apod_directory}log.txt'
+        apod.log_days_file = f'{apod.apod_directory}log_days.csv'
+        if not os.path.isfile(apod.log_days_file):
             with open(apod.log_days_file, 'w') as file:
                 file.write('date;title;link\n')
         apod.set_url_with_date(date)
         apod.download_and_save_image(set_wallpaper=True)
         return "done", 200
     except Exception as ex:
-        message = "Error: {}".format(ex)
+        message = f'Error: {ex}'
         apod.log_messages(message=message)
         return message, 500
+
 
 @app.route("/apod")
 @app.route("/apod/<string:date>")
@@ -134,7 +138,8 @@ def apod(date=None):
         apod_data = jsonify(apod.get_apod_data())
         return apod_data, 200
     except Exception as ex:
-        message = "Error: {}".format(ex)
+        message = f'Error: {ex}'
         return message, 500
+
 
 app.run()
